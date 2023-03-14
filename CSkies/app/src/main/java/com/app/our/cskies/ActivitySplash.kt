141e8a9dd59b04bfb844fe1b6af7a82d726daa2a
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-03-14 23:10:59 +0200

    Screan Show Data

---
+++ CSkies/app/src/main/java/com/app/our/cskies/ActivitySplash.kt
@@ -0,0 +1,11 @@
+package com.app.our.cskies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySplash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
