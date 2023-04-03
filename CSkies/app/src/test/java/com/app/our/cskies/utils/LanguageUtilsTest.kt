c434228473112d7f2a0ead2e8cac8b00eb709915
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-04-03 22:33:58 +0200

    testing TimeUtils

--- CSkies/app/src/test/java/com/app/our/cskies/utils/LanguageUtilsTest.kt
+++ CSkies/app/src/test/java/com/app/our/cskies/utils/LanguageUtilsTest.kt
@@ -3,10 +3,19 @@ package com.app.our.cskies.utils
 import org.junit.Assert.*
 
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(JUnit4::class)
 class LanguageUtilsTest {
 
     @Test
     fun get_En_To_Ar_Numbers() {
+        //Given
+        var num="1234567890"
+        //When
+        num=LanguageUtils.get_En_To_Ar_Numbers(num)
+        //Then
+        assertEquals(num,"١٢٣٤٥٦٧٨٩٠")
     }
 }
--- CSkies/app/src/test/java/com/app/our/cskies/utils/TimeUtilsTest.kt
+++ CSkies/app/src/test/java/com/app/our/cskies/utils/TimeUtilsTest.kt
@@ -5,8 +5,45 @@ import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.text.SimpleDateFormat
+import java.util.*
 
 @RunWith(JUnit4::class)
 class TimeUtilsTest {
-
+    @Test
+    fun formatDayTime_convertLongDayTimeToString_returnTimeAs_hh_mm_aa(){
+        //Given
+        val dt=	1680546427L
+        //When
+        val res=TimeUtils.formatDayTime(dt)
+        //Then
+        assertEquals(res,"08:27 PM")
+    }
+    @Test
+    fun getDate_convertLongDateToString_returnDataAs_dd_mm_yyyy(){
+        //Given
+        val dt=	1680546427L
+        //When
+        val res=TimeUtils.getDate(dt)
+        //Then
+        assertEquals(res,"03-04-2023")
+    }
+    @Test
+    fun getDay_ConvertLongToDay_returnDayName(){
+        //Given
+        val dt=	1680546427L
+        //When
+        val res=TimeUtils.getDay(dt)
+        //Then
+        assertEquals(res,"Monday")
+    }
+    @Test
+    fun getHour_ConvertLongToHour_returnHour(){
+        //Given
+        val dt=	1680546427L
+        //When
+        val res=TimeUtils.getHour(dt)
+        //Then
+        assertEquals(res,"08 PM")
+    }
 }
